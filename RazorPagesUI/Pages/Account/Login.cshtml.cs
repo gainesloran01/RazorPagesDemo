31b10330177a5634d9f3b6f693c19f146108541b
gainesloran01 <coderloran@gmail.com>
2022-05-18 10:15:23 -0500

    Add project files.

---
+++ RazorPagesUI/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesUI.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+    public class Credential
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
+
